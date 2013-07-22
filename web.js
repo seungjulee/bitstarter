878985ede90d808804562f51e757ffa97ff36be2
SJ Lee <seungju.sj.lee@gmail.com>
2013-07-22 13:34:11 +0000

    Modified to include the contents to web.js

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 app.use(express.logger());
 
+var index1 = fs.readFileSync(index.html);
+var buf = new Buffer(fs);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf);
 });
 
 var port = process.env.PORT || 5000;
